8f0a18d11edb7df2a612b6bbc2d355f1aa4e2904
Joey Li <joeyzaozaoli@gmail.com>
2017-11-26 07:41:20 -0800

    Add companies to database schema

--- server/controllers/processes.js
+++ server/controllers/processes.js
@@ -1,7 +1,7 @@
 const db = require('../db/config');
 
 module.exports.getAllProcesses = (req, res) => {
-  db.query('SELECT * from processes', (err, rows) => {
+  db.query('SELECT * from processes WHERE company_id=2;', (err, rows) => {
     if (!err) { res.json(rows); }
   });
 };
--- server/db/schema.sql
+++ server/db/schema.sql
@@ -4,20 +4,31 @@ CREATE DATABASE constance;
 
 USE constance;
 
+CREATE TABLE companies (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(35),
+  UNIQUE (name),
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE processes (
   id INT NOT NULL AUTO_INCREMENT,
+  company_id INT,
   acronym VARCHAR(5),
   expansion VARCHAR(45),
   overview TEXT,
   UNIQUE (acronym),
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  FOREIGN KEY (company_id) REFERENCES companies(id) ON DELETE CASCADE
 );
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * *
   Dummy Data
 * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-INSERT INTO processes(acronym, expansion, overview) VALUES
-('HRP', 'Human Resources & Payroll', 'The Company has a hiring process in place to ensure quality of talents.'),
-('PTP', 'Procure-to-Pay', 'The Company maintains an authorization matrix for approvals of purchase requisitions and purchase orders.'),
-('TCM', 'Treasury & Cash Management', 'Investments are required to be in compliance with the Company\'s Investment Policy.');
+INSERT INTO companies(name) VALUES ('Apple'), ('Orange');
+
+INSERT INTO processes(company_id, acronym, expansion, overview) VALUES
+(2, 'HRP', 'Human Resources & Payroll', 'The Company has a hiring process in place to ensure quality of talents.'),
+(2, 'PTP', 'Procure-to-Pay', 'The Company maintains an authorization matrix for approvals of purchase requisitions and purchase orders.'),
+(2, 'TCM', 'Treasury & Cash Management', 'Investments are required to be in compliance with the Company\'s Investment Policy.');
